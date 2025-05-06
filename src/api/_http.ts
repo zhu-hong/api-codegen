7909cb2618cc09550701476bb4429ea6b8e77090
朱鸿 <zhxhyy@qq.com>
2025-05-06 18:56:01 +0800

    chore

--- src/api/_http.ts
+++ src/api/_http.ts
@@ -1,70 +1,84 @@
-import axios, { AxiosError } from 'axios'
+import axios from 'axios'
 import type { AxiosRequestConfig, AxiosResponse } from 'axios'
 
 const AXIOS_INSTANCE = axios.create({
-	baseURL: '',
 	timeout: 60000,
 })
 
-AXIOS_INSTANCE.interceptors.request.use((config) => {
-	return config
-})
-
 AXIOS_INSTANCE.interceptors.response.use(
-	(response) => {
-		return response
-	},
-	(response) => {
-		if (!(response instanceof AxiosError)) {
-			console.log('未知错误')
-			throw response
+	(response) => response,
+	(res) => {
+		const { response, status } = res
+
+		if (response === undefined) {
+			console.log('服务错误')
+			throw res
 		}
 
-		const { status, message } = response
-
-		if (status && (status < 400 || status >= 600)) return response
-
-		// 处理 400/500 类错误
-
-		if (status === 401) {
-			console.log('登录失效')
-			throw response
+		if (status >= 400 && status < 600) {
+			if (typeof response?.data === 'string') {
+				console.log(response?.data)
+			} else {
+				console.log(`网络错误：（${status}），请重试`)
+			}
+			throw res
 		}
 
-		console.log(message ?? '网络错误')
-		throw response
+		return res
 	},
 )
 
-export const _http = async <T>(
+export const _http = async <T extends unknown>(
 	config: AxiosRequestConfig,
-	_config?: AxiosRequestConfig,
-): Promise<{
-	data: T
-	httpStatus: AxiosResponse['status']
-	axiosResponse: Omit<AxiosResponse, 'data' | 'status'>
-}> => {
+	_config: AxiosRequestConfig = {},
+): Promise<
+	T & {
+		_blob?: Blob
+		httpStatus: AxiosResponse['status']
+		axiosResponse: Omit<AxiosResponse, 'data' | 'status'>
+	}
+> => {
 	const $config = {
 		...config,
 		..._config,
 	}
 
-	const response = await AXIOS_INSTANCE($config)
+	const {
+		data,
+		status: httpStatus,
+		config: axiosConfig,
+		headers,
+		statusText,
+		request,
+	} = await AXIOS_INSTANCE($config)
 
-	const { data, status: httpStatus, ...axiosResponse } = response
-
-	// 处理业务code错误
-	const code = data?.code
-
-	// 正常响应或者是文件流之类
-	if (code === 0 || code === undefined || code === null) {
+	if (_config.responseType === 'blob') {
+		// @ts-ignore
 		return {
-			data,
+			_blob: data,
 			httpStatus,
-			axiosResponse,
+			axiosResponse: {
+				config: axiosConfig,
+				headers,
+				statusText,
+				request,
+			},
 		}
 	}
 
-	console.log(data?.msg || '服务错误')
-	throw data ?? response
+	if (data.code !== undefined && data.code !== 0) {
+		console.log(data.message ?? '接口错误')
+		throw data
+	}
+
+	return {
+		...data,
+		httpStatus,
+		axiosResponse: {
+			config: axiosConfig,
+			headers,
+			statusText,
+			request,
+		},
+	}
 }
